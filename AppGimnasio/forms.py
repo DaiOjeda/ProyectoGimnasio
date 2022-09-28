b6250d262a7b26383769bd1cceaa962c0fcaca8f
cgrebay <cgrebay@gmail.com>
2022-09-28 17:34:20 -0300

    Se crea rama cris2

--- AppGimnasio/forms.py
+++ AppGimnasio/forms.py
@@ -4,6 +4,6 @@ class ClienteFormulario(forms.Form):
     nombre = forms.CharField(max_length=40)
     dni = forms.CharField(max_length=8)
 
-# class ActividadFormulario(forms.Form):
-#     nombre = forms.CharField(max_length=40)
-#     dni = forms.CharField(max_length=8)
+class ActividadFormulario(forms.Form):
+  nombre = forms.CharField(max_length=40)
+  dni = forms.CharField(max_length=8)
